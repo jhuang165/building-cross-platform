9ebfbd63c50bddf58e6610b1df2ba4143694bb36
jeffreyapp <Jianlihuang@yahoo.com>
2016-01-19 21:11:14 -0600

    finished ternarys

---
+++ Downloads/building-cross-platform-class-week-2/app/utils/conditionals-ternary.js
@@ -0,0 +1,12 @@
+/*
+ * export a default function which takes two numbers,
+ * and if the first is greater than the second, it returns
+ * the first, else the second.
+ *
+ * Use a ternary operator to do this.
+ */
+
+export function ternary(a, b) {
+  var s = a > b ? b : a;
+  return s;
+}
